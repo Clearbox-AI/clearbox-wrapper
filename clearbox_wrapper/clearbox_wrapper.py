d8f02d01378ef2ac3fc0cea3fcfdc95e16180eb0
iu-lab <andrea.minieri91@gmail.com>
2020-10-22 18:45:28 +0200

    Refactored data_cleaning and preprocessing. Add tests for getting cleaned data and preprocessed data.

--- clearbox_wrapper/clearbox_wrapper.py
+++ clearbox_wrapper/clearbox_wrapper.py
@@ -1,37 +1,134 @@
+import os
+from typing import Any, Optional, Union, Callable
+
+import cloudpickle
+
+import numpy as np
+import pandas as pd
+
 import mlflow.pyfunc
 
-from typing import Any, Optional
+
+def save_model(
+    path: str,
+    model: Any,
+    preprocessing: Optional[Callable] = None,
+    data_cleaning: Optional[Callable] = None,
+) -> "ClearboxWrapper":
+    wrapped_model = ClearboxWrapper(model, preprocessing, data_cleaning)
+    wrapped_model.save(path)
+
+    if preprocessing is not None:
+        def preprocessing_function(data: Any) -> Union[pd.DataFrame, np.ndarray]:
+            preprocessed_data = (
+                preprocessing.transform(data)
+                if "transform" in dir(preprocessing)
+                else preprocessing(data)
+            )
+            return preprocessed_data
+
+        saved_preprocessing_subpath = "preprocessing.pkl"
+        with open(
+            os.path.join(path, saved_preprocessing_subpath), "wb"
+        ) as preprocessing_output_file:
+            cloudpickle.dump(preprocessing_function, preprocessing_output_file)
+
+    if data_cleaning is not None:
+        def data_cleaning_function(data: Any) -> Union[pd.DataFrame, np.ndarray]:
+            cleaned_data = (
+                data_cleaning.transform(data)
+                if "transform" in dir(data_cleaning)
+                else data_cleaning(data)
+            )
+            return cleaned_data
+
+        saved_data_cleaning_subpath = "data_cleaning.pkl"
+        with open(
+            os.path.join(path, saved_data_cleaning_subpath), "wb"
+        ) as data_cleaning_output_file:
+            cloudpickle.dump(data_cleaning_function, data_cleaning_output_file)
+
+    return wrapped_model
+
+
+def load_model(path: str):
+    loaded_model = mlflow.pyfunc.load_model(path)
+    return loaded_model
+
+
+def load_model_preprocessing(path: str):
+    loaded_model = mlflow.pyfunc.load_model(path)
+
+    saved_preprocessing_path = os.path.join(path, "preprocessing.pkl")
+    if os.path.isfile(saved_preprocessing_path):
+        with open(saved_preprocessing_path, "rb") as preprocessing_file:
+            loaded_preprocessing = cloudpickle.load(preprocessing_file)
+    else:
+        raise FileNotFoundError("There is no 'preprocessing' for this model.")
+
+    return loaded_model, loaded_preprocessing
+
+
+def load_model_preprocessing_data_cleaning(path: str):
+    loaded_model = mlflow.pyfunc.load_model(path)
+
+    saved_preprocessing_path = os.path.join(path, "preprocessing.pkl")
+    if os.path.isfile(saved_preprocessing_path):
+        with open(saved_preprocessing_path, "rb") as preprocessing_file:
+            loaded_preprocessing = cloudpickle.load(preprocessing_file)
+    else:
+        raise FileNotFoundError("There is no 'preprocessing' for this model.")
+
+    saved_data_cleaning_path = os.path.join(path, "data_cleaning.pkl")
+    if os.path.isfile(saved_data_cleaning_path):
+        with open(saved_data_cleaning_path, "rb") as data_cleaning_file:
+            loaded_data_cleaning = cloudpickle.load(data_cleaning_file)
+    else:
+        raise FileNotFoundError("There is no 'data_cleaning' for this model.")
+
+    return loaded_model, loaded_preprocessing, loaded_data_cleaning
 
 
 class ClearboxWrapper(mlflow.pyfunc.PythonModel):
     def __init__(
         self,
         model: Any,
-        preprocessing: Optional[Any] = None,
-        additional_preprocessing: Optional[Any] = None,
+        preprocessing: Optional[Callable] = None,
+        data_cleaning: Optional[Callable] = None,
     ) -> "ClearboxWrapper":
-        if preprocessing is None and additional_preprocessing is not None:
+        if preprocessing is None and data_cleaning is not None:
             raise ValueError(
                 "Attribute 'preprocessing' is None but attribute "
-                "'additional_preprocessing' is not None. If you have a single step "
+                "'data_cleaning' is not None. If you have a single step "
                 "preprocessing, pass it as attribute 'preprocessing'"
             )
         self.model = model
         self.preprocessing = preprocessing
-        self.additional_preprocessing = additional_preprocessing
+        self.data_cleaning = data_cleaning
+
+    def preprocess(self, data: Any) -> Union[pd.DataFrame, np.ndarray]:
+        if self.preprocessing is not None:
+            preprocessed_data = (
+                self.preprocessing.transform(data)
+                if "transform" in dir(self.preprocessing)
+                else self.preprocessing(data)
+            )
+            return preprocessed_data
+        else:
+            raise Exception("There is no preprocessing for this model.")
 
     def predict(self, context=None, model_input=None):
+        if self.data_cleaning is not None:
+            model_input = (
+                self.data_cleaning.transform(model_input)
+                if "transform" in dir(self.data_cleaning)
+                else self.data_cleaning(model_input)
+            )
         if self.preprocessing is not None:
             model_input = (
                 self.preprocessing.transform(model_input)
                 if "transform" in dir(self.preprocessing)
                 else self.preprocessing(model_input)
-            )
-        if self.additional_preprocessing is not None:
-            model_input = (
-                self.additional_preprocessing.transform(model_input)
-                if "transform" in dir(self.additional_preprocessing)
-                else self.additional_preprocessing(model_input)
             )
         return self.model.predict_proba(model_input)
 
--- tests/sklearn/test_sklearn_iris.py
+++ tests/sklearn/test_sklearn_iris.py
@@ -9,11 +9,9 @@ import sklearn.neighbors as neighbors
 import sklearn.tree as tree
 import sklearn.ensemble as ensemble
 
-import sklearn.preprocessing as preprocessing
+import sklearn.preprocessing as sk_preprocessing
 
-import mlflow.pyfunc
-
-from clearbox_wrapper.clearbox_wrapper import ClearboxWrapper
+import clearbox_wrapper.clearbox_wrapper as cbw
 
 
 @pytest.fixture(scope="module")
@@ -29,7 +27,9 @@ def sk_function_transformer():
     def simple_preprocessor(numpy_x):
         return numpy_x ** 2
 
-    transformer = preprocessing.FunctionTransformer(simple_preprocessor, validate=True)
+    transformer = sk_preprocessing.FunctionTransformer(
+        simple_preprocessor, validate=True
+    )
     return transformer
 
 
@@ -52,59 +52,45 @@ def drop_column_transformer():
 
 
 @pytest.mark.parametrize(
-    "sklearn_model, data",
+    "sklearn_model",
     [
-        (linear_model.LogisticRegression(), pytest.lazy_fixture("iris_data")),
-        (svm.SVC(probability=True), pytest.lazy_fixture("iris_data")),
-        (neighbors.KNeighborsClassifier(), pytest.lazy_fixture("iris_data")),
-        (tree.DecisionTreeClassifier(), pytest.lazy_fixture("iris_data")),
-        (ensemble.RandomForestClassifier(), pytest.lazy_fixture("iris_data")),
+        (linear_model.LogisticRegression()),
+        (svm.SVC(probability=True)),
+        (neighbors.KNeighborsClassifier()),
+        (tree.DecisionTreeClassifier()),
+        (ensemble.RandomForestClassifier()),
     ],
 )
-def test_iris_sklearn_no_preprocessing(sklearn_model, data):
-    x, y = data
+def test_iris_sklearn_no_preprocessing(sklearn_model, iris_data):
+    x, y = iris_data
     fitted_model = sklearn_model.fit(x, y)
-    wrapped_model = ClearboxWrapper(fitted_model)
+    wrapped_model = cbw.ClearboxWrapper(fitted_model)
     original_model_predictions = fitted_model.predict_proba(x[:5])
     wrapped_model_predictions = wrapped_model.predict(model_input=x[:5])
     np.testing.assert_array_equal(original_model_predictions, wrapped_model_predictions)
 
 
 @pytest.mark.parametrize(
-    "sklearn_model, sk_transformer, data",
+    "sklearn_model, sk_transformer",
     [
-        (
-            linear_model.LogisticRegression(),
-            preprocessing.StandardScaler(),
-            pytest.lazy_fixture("iris_data"),
-        ),
+        (linear_model.LogisticRegression(), sk_preprocessing.StandardScaler()),
         (
             svm.SVC(probability=True),
-            preprocessing.QuantileTransformer(random_state=0),
-            pytest.lazy_fixture("iris_data"),
+            sk_preprocessing.QuantileTransformer(random_state=0),
         ),
         (
             neighbors.KNeighborsClassifier(),
-            preprocessing.KBinsDiscretizer(n_bins=5, encode="ordinal"),
-            pytest.lazy_fixture("iris_data"),
+            sk_preprocessing.KBinsDiscretizer(n_bins=5, encode="ordinal"),
         ),
-        (
-            tree.DecisionTreeClassifier(),
-            preprocessing.RobustScaler(),
-            pytest.lazy_fixture("iris_data"),
-        ),
-        (
-            ensemble.RandomForestClassifier(),
-            preprocessing.MaxAbsScaler(),
-            pytest.lazy_fixture("iris_data"),
-        ),
+        (tree.DecisionTreeClassifier(), sk_preprocessing.RobustScaler()),
+        (ensemble.RandomForestClassifier(), sk_preprocessing.MaxAbsScaler()),
     ],
 )
-def test_iris_sklearn_preprocessing(sklearn_model, sk_transformer, data):
-    x, y = data
+def test_iris_sklearn_preprocessing(sklearn_model, sk_transformer, iris_data):
+    x, y = iris_data
     x_transformed = sk_transformer.fit_transform(x)
     fitted_model = sklearn_model.fit(x_transformed, y)
-    wrapped_model = ClearboxWrapper(fitted_model, sk_transformer)
+    wrapped_model = cbw.ClearboxWrapper(fitted_model, sk_transformer)
     original_model_predictions = fitted_model.predict_proba(
         sk_transformer.transform(x[:5])
     )
@@ -113,130 +99,295 @@ def test_iris_sklearn_preprocessing(sklearn_model, sk_transformer, data):
 
 
 @pytest.mark.parametrize(
-    "sklearn_model, function_transformer, data",
+    "sklearn_model",
     [
-        (
-            linear_model.LogisticRegression(),
-            pytest.lazy_fixture("sk_function_transformer"),
-            pytest.lazy_fixture("iris_data"),
-        ),
-        (
-            svm.SVC(probability=True),
-            pytest.lazy_fixture("sk_function_transformer"),
-            pytest.lazy_fixture("iris_data"),
-        ),
-        (
-            neighbors.KNeighborsClassifier(),
-            pytest.lazy_fixture("sk_function_transformer"),
-            pytest.lazy_fixture("iris_data"),
-        ),
-        (
-            tree.DecisionTreeClassifier(),
-            pytest.lazy_fixture("sk_function_transformer"),
-            pytest.lazy_fixture("iris_data"),
-        ),
-        (
-            ensemble.RandomForestClassifier(),
-            pytest.lazy_fixture("sk_function_transformer"),
-            pytest.lazy_fixture("iris_data"),
-        ),
+        (linear_model.LogisticRegression()),
+        (svm.SVC(probability=True)),
+        (neighbors.KNeighborsClassifier()),
+        (tree.DecisionTreeClassifier()),
+        (ensemble.RandomForestClassifier()),
     ],
 )
 def test_iris_sklearn_preprocessing_with_function_transformer(
-    sklearn_model, function_transformer, data
+    sklearn_model, sk_function_transformer, iris_data
 ):
-    x, y = data
-    x_transformed = function_transformer.fit_transform(x)
+    x, y = iris_data
+    x_transformed = sk_function_transformer.fit_transform(x)
     fitted_model = sklearn_model.fit(x_transformed, y)
-    wrapped_model = ClearboxWrapper(fitted_model, function_transformer)
+    wrapped_model = cbw.ClearboxWrapper(fitted_model, sk_function_transformer)
     original_model_predictions = fitted_model.predict_proba(x_transformed[:5])
     wrapped_model_predictions = wrapped_model.predict(model_input=x[:5])
     np.testing.assert_array_equal(original_model_predictions, wrapped_model_predictions)
 
 
 @pytest.mark.parametrize(
-    "sklearn_model, custom_preprocessing, data",
+    "sklearn_model",
     [
-        (
-            linear_model.LogisticRegression(),
-            pytest.lazy_fixture("custom_transformer"),
-            pytest.lazy_fixture("iris_data"),
-        ),
-        (
-            svm.SVC(probability=True),
-            pytest.lazy_fixture("custom_transformer"),
-            pytest.lazy_fixture("iris_data"),
-        ),
-        (
-            neighbors.KNeighborsClassifier(),
-            pytest.lazy_fixture("custom_transformer"),
-            pytest.lazy_fixture("iris_data"),
-        ),
-        (
-            tree.DecisionTreeClassifier(),
-            pytest.lazy_fixture("custom_transformer"),
-            pytest.lazy_fixture("iris_data"),
-        ),
-        (
-            ensemble.RandomForestClassifier(),
-            pytest.lazy_fixture("custom_transformer"),
-            pytest.lazy_fixture("iris_data"),
-        ),
+        (linear_model.LogisticRegression()),
+        (svm.SVC(probability=True)),
+        (neighbors.KNeighborsClassifier()),
+        (tree.DecisionTreeClassifier()),
+        (ensemble.RandomForestClassifier()),
     ],
 )
 def test_iris_sklearn_preprocessing_with_custom_transformer(
-    sklearn_model, custom_preprocessing, data
+    sklearn_model, custom_transformer, iris_data
 ):
-    x, y = data
-    x_transformed = custom_preprocessing(x)
+    x, y = iris_data
+    x_transformed = custom_transformer(x)
     fitted_model = sklearn_model.fit(x_transformed, y)
-    wrapped_model = ClearboxWrapper(fitted_model, custom_preprocessing)
+    wrapped_model = cbw.ClearboxWrapper(fitted_model, custom_transformer)
     original_model_predictions = fitted_model.predict_proba(x_transformed[:5])
     wrapped_model_predictions = wrapped_model.predict(model_input=x[:5])
     np.testing.assert_array_equal(original_model_predictions, wrapped_model_predictions)
 
 
 @pytest.mark.parametrize(
-    "sklearn_model, data",
+    "sklearn_model, preprocessor",
     [
-        (linear_model.LogisticRegression(), pytest.lazy_fixture("iris_data")),
-        (svm.SVC(probability=True), pytest.lazy_fixture("iris_data")),
-        (neighbors.KNeighborsClassifier(), pytest.lazy_fixture("iris_data")),
-        (tree.DecisionTreeClassifier(), pytest.lazy_fixture("iris_data")),
-        (ensemble.RandomForestClassifier(), pytest.lazy_fixture("iris_data")),
+        (linear_model.LogisticRegression(), sk_preprocessing.StandardScaler()),
+        (
+            svm.SVC(probability=True),
+            sk_preprocessing.QuantileTransformer(random_state=0),
+        ),
+        (
+            neighbors.KNeighborsClassifier(),
+            sk_preprocessing.KBinsDiscretizer(n_bins=5, encode="ordinal"),
+        ),
+        (tree.DecisionTreeClassifier(), sk_preprocessing.RobustScaler()),
+        (ensemble.RandomForestClassifier(), sk_preprocessing.MaxAbsScaler()),
     ],
 )
-def test_iris_sklearn_no_preprocessing_save(sklearn_model, data, tmpdir):
-    x, y = data
+def test_iris_sklearn_data_cleaning_and_preprocessing(
+    sklearn_model, preprocessor, iris_data, drop_column_transformer, tmpdir
+):
+    x, y = iris_data
+    data_cleaning = drop_column_transformer
+    x_transformed = data_cleaning(x)
+    x_transformed = preprocessor.fit_transform(x_transformed)
+    fitted_model = sklearn_model.fit(x_transformed, y)
+    wrapped_model = cbw.ClearboxWrapper(fitted_model, preprocessor, data_cleaning)
+    original_model_predictions = fitted_model.predict_proba(x_transformed[:5])
+    wrapped_model_predictions = wrapped_model.predict(model_input=x[:5])
+    np.testing.assert_array_equal(original_model_predictions, wrapped_model_predictions)
+
+
+def test_iris_sklearn_data_cleaning_without_preprocessing(iris_data):
+    x, y = iris_data
+    sk_transformer = sk_preprocessing.StandardScaler()
+    model = linear_model.LogisticRegression()
+    x_transformed = sk_transformer.fit_transform(x)
+    fitted_model = model.fit(x_transformed, y)
+    with pytest.raises(ValueError):
+        cbw.ClearboxWrapper(fitted_model, data_cleaning=sk_transformer)
+
+
+@pytest.mark.parametrize(
+    "sklearn_model",
+    [
+        (linear_model.LogisticRegression()),
+        (svm.SVC(probability=True)),
+        (neighbors.KNeighborsClassifier()),
+        (tree.DecisionTreeClassifier()),
+        (ensemble.RandomForestClassifier()),
+    ],
+)
+def test_iris_sklearn_no_preprocessing_save_and_load(sklearn_model, iris_data, tmpdir):
+    x, y = iris_data
     fitted_model = sklearn_model.fit(x, y)
-    wrapped_model = ClearboxWrapper(fitted_model)
-    tmp_model_path = tmpdir + "/saved_model"
-    wrapped_model.save(tmp_model_path)
-    loaded_model = mlflow.pyfunc.load_model(str(tmp_model_path))
+    tmp_model_path = str(tmpdir + "/saved_model")
+    cbw.save_model(tmp_model_path, fitted_model)
+    loaded_model = cbw.load_model(tmp_model_path)
     original_model_predictions = fitted_model.predict_proba(x[:5])
     loaded_model_predictions = loaded_model.predict(x[:5])
     np.testing.assert_array_equal(original_model_predictions, loaded_model_predictions)
 
 
-def test_iris_sklearn_additional_preprocessing_without_preprocessing(iris_data):
+@pytest.mark.parametrize(
+    "sklearn_model, preprocessor",
+    [
+        (linear_model.LogisticRegression(), sk_preprocessing.StandardScaler()),
+        (
+            svm.SVC(probability=True),
+            sk_preprocessing.QuantileTransformer(random_state=0),
+        ),
+        (
+            neighbors.KNeighborsClassifier(),
+            sk_preprocessing.KBinsDiscretizer(n_bins=5, encode="ordinal"),
+        ),
+        (tree.DecisionTreeClassifier(), sk_preprocessing.RobustScaler()),
+        (ensemble.RandomForestClassifier(), sk_preprocessing.MaxAbsScaler()),
+    ],
+)
+def test_iris_sklearn_preprocessing_save_and_load(
+    sklearn_model, preprocessor, iris_data, tmpdir
+):
     x, y = iris_data
-    sk_transformer = preprocessing.StandardScaler()
+    x_transformed = preprocessor.fit_transform(x)
+    fitted_model = sklearn_model.fit(x_transformed, y)
+    tmp_model_path = str(tmpdir + "/saved_model")
+    cbw.save_model(tmp_model_path, fitted_model, preprocessor)
+    loaded_model = cbw.load_model(tmp_model_path)
+    original_model_predictions = fitted_model.predict_proba(x_transformed[:5])
+    loaded_model_predictions = loaded_model.predict(x[:5])
+    np.testing.assert_array_equal(original_model_predictions, loaded_model_predictions)
+
+
+@pytest.mark.parametrize(
+    "sklearn_model, preprocessor",
+    [
+        (linear_model.LogisticRegression(), sk_preprocessing.StandardScaler()),
+        (
+            svm.SVC(probability=True),
+            sk_preprocessing.QuantileTransformer(random_state=0),
+        ),
+        (
+            neighbors.KNeighborsClassifier(),
+            sk_preprocessing.KBinsDiscretizer(n_bins=5, encode="ordinal"),
+        ),
+        (tree.DecisionTreeClassifier(), sk_preprocessing.RobustScaler()),
+        (ensemble.RandomForestClassifier(), sk_preprocessing.MaxAbsScaler()),
+    ],
+)
+def test_iris_sklearn_data_cleaning_and_preprocessing_save_and_load(
+    sklearn_model, preprocessor, iris_data, drop_column_transformer, tmpdir
+):
+    x, y = iris_data
+    data_cleaning = drop_column_transformer
+    x_transformed = data_cleaning(x)
+    x_transformed = preprocessor.fit_transform(x_transformed)
+    fitted_model = sklearn_model.fit(x_transformed, y)
+    tmp_model_path = str(tmpdir + "/saved_model")
+    cbw.save_model(tmp_model_path, fitted_model, preprocessor, data_cleaning)
+    loaded_model = cbw.load_model(tmp_model_path)
+    original_model_predictions = fitted_model.predict_proba(x_transformed[:5])
+    loaded_model_predictions = loaded_model.predict(x[:5])
+    np.testing.assert_array_equal(original_model_predictions, loaded_model_predictions)
+
+
+def test_iris_sklearn_load_preprocessing_without_preprocessing(iris_data, tmpdir):
+    x, y = iris_data
+    model = linear_model.LogisticRegression()
+    fitted_model = model.fit(x, y)
+    tmp_model_path = str(tmpdir + "/saved_model")
+    cbw.save_model(tmp_model_path, fitted_model)
+    with pytest.raises(FileNotFoundError):
+        loaded_model, preprocessing = cbw.load_model_preprocessing(tmp_model_path)
+
+
+def test_iris_sklearn_load_data_cleaning_without_data_cleaning(iris_data, tmpdir):
+    x, y = iris_data
+    sk_transformer = sk_preprocessing.StandardScaler()
     model = linear_model.LogisticRegression()
     x_transformed = sk_transformer.fit_transform(x)
     fitted_model = model.fit(x_transformed, y)
-    with pytest.raises(ValueError):
-        ClearboxWrapper(fitted_model, additional_preprocessing=sk_transformer)
+    tmp_model_path = str(tmpdir + "/saved_model")
+    cbw.save_model(tmp_model_path, fitted_model, sk_transformer)
+    with pytest.raises(FileNotFoundError):
+        (
+            loaded_model,
+            preprocessing,
+            data_cleaning,
+        ) = cbw.load_model_preprocessing_data_cleaning(tmp_model_path)
 
 
-def test_iris_sklearn_two_step_preprocessing(iris_data, drop_column_transformer):
+@pytest.mark.parametrize(
+    "sklearn_model, preprocessor",
+    [
+        (linear_model.LogisticRegression(), sk_preprocessing.StandardScaler()),
+        (
+            svm.SVC(probability=True),
+            sk_preprocessing.QuantileTransformer(random_state=0),
+        ),
+        (
+            neighbors.KNeighborsClassifier(),
+            sk_preprocessing.KBinsDiscretizer(n_bins=5, encode="ordinal"),
+        ),
+        (tree.DecisionTreeClassifier(), sk_preprocessing.RobustScaler()),
+        (ensemble.RandomForestClassifier(), sk_preprocessing.MaxAbsScaler()),
+    ],
+)
+def test_iris_sklearn_get_preprocessed_data(
+    sklearn_model, preprocessor, iris_data, tmpdir
+):
     x, y = iris_data
-    first_preprocessor = drop_column_transformer
-    second_preprocessor = preprocessing.StandardScaler()
-    model = linear_model.LogisticRegression()
-    x_transformed = first_preprocessor(x)
-    x_transformed = second_preprocessor.fit_transform(x_transformed)
-    fitted_model = model.fit(x_transformed, y)
-    wrapped_model = ClearboxWrapper(fitted_model, first_preprocessor, second_preprocessor)
-    original_model_predictions = fitted_model.predict_proba(x_transformed[:5])
-    wrapped_model_predictions = wrapped_model.predict(model_input=x[:5])
-    np.testing.assert_array_equal(original_model_predictions, wrapped_model_predictions)
+    x_transformed = preprocessor.fit_transform(x)
+    fitted_model = sklearn_model.fit(x_transformed, y)
+    tmp_model_path = str(tmpdir + "/saved_model")
+    cbw.save_model(tmp_model_path, fitted_model, preprocessor)
+    loaded_model, loaded_preprocessing = cbw.load_model_preprocessing(tmp_model_path)
+    x_transformed_by_loaded_preprocessing = loaded_preprocessing(x)
+    np.testing.assert_array_equal(x_transformed, x_transformed_by_loaded_preprocessing)
+
+
+@pytest.mark.parametrize(
+    "sklearn_model, preprocessor",
+    [
+        (linear_model.LogisticRegression(), sk_preprocessing.StandardScaler()),
+        (
+            svm.SVC(probability=True),
+            sk_preprocessing.QuantileTransformer(random_state=0),
+        ),
+        (
+            neighbors.KNeighborsClassifier(),
+            sk_preprocessing.KBinsDiscretizer(n_bins=5, encode="ordinal"),
+        ),
+        (tree.DecisionTreeClassifier(), sk_preprocessing.RobustScaler()),
+        (ensemble.RandomForestClassifier(), sk_preprocessing.MaxAbsScaler()),
+    ],
+)
+def test_iris_sklearn_get_cleaned_data(
+    sklearn_model, preprocessor, iris_data, drop_column_transformer, tmpdir
+):
+    x, y = iris_data
+    data_cleaner = drop_column_transformer
+    x_cleaned = data_cleaner(x)
+    x_transformed = preprocessor.fit_transform(x_cleaned)
+    fitted_model = sklearn_model.fit(x_transformed, y)
+    tmp_model_path = str(tmpdir + "/saved_model")
+    cbw.save_model(tmp_model_path, fitted_model, preprocessor, data_cleaner)
+    (
+        loaded_model,
+        loaded_preprocessing,
+        loaded_data_cleaning,
+    ) = cbw.load_model_preprocessing_data_cleaning(tmp_model_path)
+    x_cleaned_by_loaded_data_cleaning = loaded_data_cleaning(x)
+    np.testing.assert_array_equal(x_cleaned, x_cleaned_by_loaded_data_cleaning)
+
+
+@pytest.mark.parametrize(
+    "sklearn_model, preprocessor",
+    [
+        (linear_model.LogisticRegression(), sk_preprocessing.StandardScaler()),
+        (
+            svm.SVC(probability=True),
+            sk_preprocessing.QuantileTransformer(random_state=0),
+        ),
+        (
+            neighbors.KNeighborsClassifier(),
+            sk_preprocessing.KBinsDiscretizer(n_bins=5, encode="ordinal"),
+        ),
+        (tree.DecisionTreeClassifier(), sk_preprocessing.RobustScaler()),
+        (ensemble.RandomForestClassifier(), sk_preprocessing.MaxAbsScaler()),
+    ],
+)
+def test_iris_sklearn_get_cleaned_and_processed_data(
+    sklearn_model, preprocessor, iris_data, drop_column_transformer, tmpdir
+):
+    x, y = iris_data
+    data_cleaner = drop_column_transformer
+    x_cleaned = data_cleaner(x)
+    x_transformed = preprocessor.fit_transform(x_cleaned)
+    fitted_model = sklearn_model.fit(x_transformed, y)
+    tmp_model_path = str(tmpdir + "/saved_model")
+    cbw.save_model(tmp_model_path, fitted_model, preprocessor, data_cleaner)
+    (
+        loaded_model,
+        loaded_preprocessing,
+        loaded_data_cleaning,
+    ) = cbw.load_model_preprocessing_data_cleaning(tmp_model_path)
+    x_cleaned_by_loaded_data_cleaning = loaded_data_cleaning(x)
+    x_transformed_by_loaded_preprocessing = loaded_preprocessing(
+        x_cleaned_by_loaded_data_cleaning
+    )
+    np.testing.assert_array_equal(x_transformed, x_transformed_by_loaded_preprocessing)
