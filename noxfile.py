84a978bb9eb372ce3b1ba56a3af1d560d8fe04da
iu-lab <andrea.minieri91@gmail.com>
2020-12-15 18:19:20 +0100

    Add nox session for python 3.6.2

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,7 @@
 import nox
 
 
-@nox.session(python=["3.7.0", "3.8.0", "3.8.5"])
+@nox.session(python=["3.6.2", "3.7.0", "3.8.0", "3.8.5"])
 def tests(session):
     args = session.posargs or ["--cov"]
     session.run("poetry", "install", external=True)
